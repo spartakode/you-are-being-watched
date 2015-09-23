a2093ad31f7e0e5578c7d2d2646d02f36b35d400
Adnan Issadeen <eddyrox1@hotmail.com>
2015-09-23 18:29:50 +0530

    Arranged files to be able to package chrome extension
    First of all, all thanks go to icon monstr for providing the icons to
    get this started. Need to update README to reflect this. Please check
    out iconmonstr at iconmonstr.com
    
    This preps the directories for building the chrome extension for the
    first time. Need to add to readme that till the grunt file is ready, it
    is necessary to copy the directory.js and matcher.js files to the chrome
    directory before packaging it.
    
    Updated directory.js file to actually contain the dictionary of
    blacklisted sites
    
    Updated manifest.json to include matcher.js and directory.js in. Need to
    test if I can simply load it in from outside of the directory so I can
    then eliminate the copying step

--- chrome/checkpage.js
+++ chrome/checkpage.js
@@ -1,6 +1,6 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
     if (changeInfo.status == "complete"){
-        if(tab.url.toString() == "https://www.heroku.com/"){
+        if(doesUrlMatchBlackList(tab.url.toString(),dictionary)){
             var result = window.confirm("Warning. This site or its owners may support Government surveillance. Click ok to continue or cancel to close tab");
             if (result === false){
                 chrome.tabs.remove(tabId);
---
+++ core/directory.js
@@ -0,0 +1,65 @@
+dictionary = {urls:
+    {
+        "heroku.com": {
+            "isDirect": false,
+            "parent": "https://salesforce.com/",
+            "source": "",
+            "description": "",
+        },
+        "salesforce.com": {
+            "isDirect": true,
+            "parent": "",
+            "source": "",
+            "description": "",
+        },
+        "microsoft.com": {
+            "isDirect": true,
+            "parent": "",
+            "source": "",
+            "description": "",
+        },
+        "adobe.com": {
+            "isDirect": true,
+            "parent": "",
+            "source": "",
+            "description": "",
+        },
+        "behance.net": {
+            "isDirect": false,
+            "parent": "adobe.com",
+            "source": "",
+            "description": "",
+        },
+        "ibm.com": {
+            "isDirect": true,
+            "parent": "",
+            "source": "",
+            "description": "",
+        },
+        "apple.com": {
+            "isDirect": true,
+            "parent": "",
+            "source": "",
+            "description": "",
+        },
+        "oracle.com": {
+            "isDirect": true,
+            "parent": "",
+            "source": "",
+            "description": "",
+        },
+        "symantec.com": {
+            "isDirect": true,
+            "parent": "",
+            "source": "",
+            "description": "",
+        },
+        "autodesk.com": {
+            "isDirect": true,
+            "parent": "",
+            "source": "",
+            "description": "",
+        },
+    }
+}
+
