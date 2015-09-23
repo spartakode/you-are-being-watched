90dc68bd8993917575e50576ab377edc2b18b94d
Adnan Issadeen <eddyrox1@hotmail.com>
2015-09-23 12:06:39 +0530

    Adds support for continuing or closing tab. Addresses #3
    Uses window.confirm to display a confirmation message instead of showing
    a simple alert. Will replace this later with an actual modal window.

--- chrome/checkpage.js
+++ chrome/checkpage.js
@@ -1,13 +1,10 @@
-chrome.tabs.onCreated.addListener(function(tab){
-    console.log(tab.id.toString() + tab.url.toString());
-});
-
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-    //alert(tab.url.toString());
-    //alert(changeInfo.status + " : "  + tab.url.toString());
     if (changeInfo.status == "complete"){
         if(tab.url.toString() == "https://www.heroku.com/"){
-            alert("Warning. This site or its owners may support Government surveillance");
+            var result = window.confirm("Warning. This site or its owners may support Government surveillance. Click ok to continue or cancel to close tab");
+            if (result === false){
+                chrome.tabs.remove(tabId);
+            }
         }
     }
 });
